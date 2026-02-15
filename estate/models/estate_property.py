56a44e65ee0084517f8edb947cd5e8a6c43e3635
A113E <amg11amg2@gmail.com>
2026-02-14 22:08:32 -0500

    construction: Basic Fields Models Init

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,70 @@
-from odoo import models
+from odoo import fields,models
 
 class EstateProperty(models.Model):
   _name = 'estate.property'
+  _description = 'Estate Property'
+  
+  name = fields.Char(
+    string='Property Name',
+    required=True
+  )
+  
+  description = fields.Text(
+    string='Description'
+  )
+  
+  postcode = fields.Char(
+    string='Postal Code'
+  )
+  
+  date_availability = fields.Date(
+    string='Date Availability'
+  )
+  
+  expected_price = fields.Float(
+    string='Expected Price',
+    required=True
+  )
+  
+  selling_price = fields.Float(
+    string='Selling Price'
+  )
+  
+  bedrooms = fields.Integer(
+    string='Bedrooms'
+  )
+  
+  living_area = fields.Integer(
+    string='Living Area'
+  )
+  
+  facades = fields.Integer(
+    string='Facades'
+  )
+  
+  garage = fields.Boolean(
+    string='Garage',
+    default=False,
+    help='Indicate if it has a garage'
+  )
+  
+  garden = fields.Boolean(
+    string='Garden',
+    default=False,
+    help='Indicate if it has a garden'
+  )
+  
+  garden_area = fields.Integer(
+    string='Garden Area'
+  )
+  
+  garden_orientation = fields.Selection(
+    string='Garden Orientation',
+    selection=[
+      ('north','North'),
+      ('south','South'),
+      ('east','East'),
+      ('west','West')
+    ],
+    help='Indicate the orientation of the garden'
+  )
