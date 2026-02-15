2f574dba7d944551f77c594a04a0457798c6d695
A113E <amg11amg2@gmail.com>
2026-02-14 21:39:35 -0500

    INIT: Initial Commit

---
+++ estate/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+  'name': 'Estate',
+  'version': '14.0.1.0.0',
+  'category': 'Sales/CRM',
+  'summary': 'Modulo para la Gestión Inmobiliaria',
+  'description': """
+  """,
+  'author': 'Alberto A. Mártir González',
+  'license': 'LGPL-3',
+  'depends': [
+    'base',
+  ],
+  'data': [],
+  'demo': [],
+  'qweb': [],
+  'images': [],
+  'installable': True,
+  'application': True,
+  'auto_install': False,
+  'active': False
+}
