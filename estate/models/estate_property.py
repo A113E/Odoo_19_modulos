7b0eec2bad62c4b0171c80d83501e5df05f8e62e
A113E <amg11amg2@gmail.com>
2026-03-14 20:14:44 -0400

    feat: New relatiosn fields: user_id and partner_id

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -10,11 +10,28 @@ class EstateProperty(models.Model):
     string='Property Name',
     required=True
   )
-  
+  # Campos relacionales
   property_type_id = fields.Many2one(
     'estate.property.type',
     string='Property Type',
     required=True
+  )
+  
+  user_id = fields.Many2one(
+    'res.users',
+    string='Salesperson',
+    index=True, # para busqueda rapida
+    tracking=True, # para registrar cambios en el chatter (True toma como valor 100 en importancia)
+    default=lambda self: self.env.user # Toma el registro del usuario actual por defecto
+  )
+  
+  partner_id = fields.Many2one(
+    'res.partner',
+    string='Buyer',
+    index=True,
+    tracking=10, #  el numero 10 hace que este sea el chatter con mas prioridad en la lista 
+    domain="['|', ('company_id','=', False), ('company_id','=', 'company_id')]", # Filtra por compradores que no tengan compañias o que pertenezcan a la compañia del documento
+    copy=False
   )
   
   description = fields.Text(
